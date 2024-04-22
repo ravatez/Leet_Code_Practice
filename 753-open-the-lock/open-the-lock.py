2d35398207e80352f12199180955cd48e56e4b2a
Rupesh Ravate <35538716+ravatez@users.noreply.github.com>
2024-04-22 13:46:13 +0530

    Time: 455 ms (53.29%) | Memory: 18 MB (50.54%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.py
@@ -0,0 +1,26 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+
+        def children(lock):
+            res = []
+            for i in range(4):
+                digit = str((int(lock[i]) + 1) % 10)
+                res.append(lock[:i] + digit + lock[i+1:])
+                digit = str((int(lock[i]) - 1 + 10) % 10)
+                res.append(lock[:i] + digit + lock[i+1:])       
+            return res     
+         
+        q = deque()
+        q.append(["0000",0]) #[lock Turns]
+        visit = set(deadends)
+        while q:
+            lock,turns = q.popleft()
+            if lock == target:
+                return turns
+            for child in children(lock):
+                if child not in visit:
+                    visit.add(child)
+                    q.append([child,turns + 1])
+        return -1
