899c2d3b8988a6856def393fc1a8da5d53ddb688
Rupesh Ravate <ravatez@gmail.com>
2024-05-15 20:05:15 +0530

    Time: 2041 ms (99.09%) | Memory: 26.9 MB (84.48%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.py
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.py
@@ -1,63 +1,39 @@
 class Solution:
-  def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
-    self.dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    n = len(grid)
-    distToThief = self._getDistToThief(grid)
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        if grid[0][0] == 1 or grid[row-1][col-1] == 1:
+            return 0
+        safeness = [[-1]*col for _ in range(row)]
+        q = deque([])
 
-    def hasValidPath(safeness: int) -> bool:
-      if distToThief[0][0] < safeness:
-        return False
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 1:
+                    safeness[r][c] = 0
+                    q.append((0, r, c))
+        
+        direction = [(-1,0),(1,0),(0,-1),(0,1)]
+        while q:
+            dis, r, c = q.popleft()
+            for dr, dc in direction:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < row and 0 <= nc < col and safeness[nr][nc] == -1:
+                    safeness[nr][nc] = dis + 1
+                    q.append((dis + 1, nr, nc))
+        
+        heap = [(-safeness[row-1][col-1], row - 1, col - 1)]
+        seen = set([(row-1,col-1)])
 
-      q = collections.deque([(0, 0)])
-      seen = {(0, 0)}
-
-      while q:
-        i, j = q.popleft()
-        if distToThief[i][j] < safeness:
-          continue
-        if i == n - 1 and j == n - 1:
-          return True
-        for dx, dy in self.dirs:
-          x = i + dx
-          y = j + dy
-          if x < 0 or x == n or y < 0 or y == n:
-            continue
-          if (x, y) in seen:
-            continue
-          q.append((x, y))
-          seen.add((x, y))
-
-      return False
-
-    return bisect.bisect_left(range(n * 2), True,
-                              key=lambda m: not hasValidPath(m)) - 1
-
-  def _getDistToThief(self, grid: List[List[int]]) -> List[List[int]]:
-    n = len(grid)
-    distToThief = [[0] * n for _ in range(n)]
-    q = collections.deque()
-    seen = set()
-
-    for i in range(n):
-      for j in range(n):
-        if grid[i][j] == 1:
-          q.append((i, j))
-          seen.add((i, j))
-
-    dist = 0
-    while q:
-      for _ in range(len(q)):
-        i, j = q.popleft()
-        distToThief[i][j] = dist
-        for dx, dy in self.dirs:
-          x = i + dx
-          y = j + dy
-          if x < 0 or x == n or y < 0 or y == n:
-            continue
-          if (x, y) in seen:
-            continue
-          q.append((x, y))
-          seen.add((x, y))
-      dist += 1
-
-    return distToThief
+        while heap:
+            dis, r, c = heappop(heap)
+            if (r, c) == (0, 0):
+                return -dis
+                
+            for dr, dc in direction:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < row and 0 <= nc < col and (nr, nc) not in seen:
+                    safe = max(dis, -safeness[nr][nc])
+                    heappush(heap, (safe, nr, nc))
+                    seen.add((nr, nc))
+        
+        return -1
