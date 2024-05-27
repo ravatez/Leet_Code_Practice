3ecd1d534d689955c20326573ed3ffe41c4b1e28
Rupesh Ravate <ravatez@gmail.com>
2024-05-27 19:38:30 +0530

    Time: 32 ms (93.93%) | Memory: 16.6 MB (48.53%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,9 +1,18 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
+        count = [0] * (len(nums) + 1)
+        for n in nums:
+            index = n if n < len(nums) else len(nums) # min(n, len(nums))
+            count[index] += 1
+        
+        total_right = 0
+        for i in reversed(range(len(nums) + 1)):
+            total_right += count[i]
+            if i == total_right:
+                return total_right
+        return -1
 
-
-
-
+""" 
 # Sorting Method
         nums.sort()
         i = 0
@@ -18,3 +27,4 @@ class Solution:
             i += 1
             total_right = len(nums) - i
         return -1
+"""
