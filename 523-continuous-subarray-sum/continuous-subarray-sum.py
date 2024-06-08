91f3c2361c1a777ad1c36b43bc596cabc55595dc
Rupesh Ravate <ravatez@gmail.com>
2024-06-08 16:38:28 +0530

    Time: 814 ms (85.09%) | Memory: 36 MB (91.22%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        map = {0: -1}  # To handle the case where the sub-array starts from index 0
+        sum = 0
+
+        for i in range(len(nums)):
+            sum += nums[i]
+            rem = sum % k
+
+            if rem in map:
+                if i - map[rem] > 1:
+                    return True
+            else:
+                map[rem] = i
+
+        return False    
