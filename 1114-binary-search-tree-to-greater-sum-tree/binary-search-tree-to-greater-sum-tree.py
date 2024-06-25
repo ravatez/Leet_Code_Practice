ca602ccbe7ccebd836bd3d4e0c92dcb62070fe1f
Rupesh Ravate <ravatez@gmail.com>
2024-06-25 15:36:37 +0530

    Time: 41 ms (21.43%) | Memory: 16.3 MB (96.70%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.sum = 0
+    def bstToGst(self, root):
+        if root:
+            self.bstToGst(root.right)  # Traverse the right subtree
+            self.sum += root.val  # Update the sum
+            root.val = self.sum  # Update the current node's value
+            self.bstToGst(root.left)  # Traverse the left subtree
+        return root
