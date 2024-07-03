d0589505e9f9cd7b008234724821fb701c70e659
Rupesh Ravate <ravatez@gmail.com>
2024-07-03 18:35:32 +0530

    Time: 280 ms (50.24%) | Memory: 27.4 MB (39.86%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -3,7 +3,17 @@ class Solution:
         if len(nums) <= 4:
             return 0
         nums.sort()
+        k = len(nums) - 3
+        ans = nums[-1] - nums[0]
+        for i in range(k - 1, len(nums)):
+            ans = min(ans, nums[i] - nums[i - k + 1])
+        return ans
+""" 
+       if len(nums) <= 4:
+            return 0
+        nums.sort()
         ans = nums[-1] - nums[0]
         for i in range(4):
             ans = min(ans, nums[-(4 - i)] - nums[i])
-        return ans                           
+        return ans 
+"""                          
