84f8eccc951cda81a50a622fc5f5cd8e5eb144c6
Rupesh Ravate <ravatez@gmail.com>
2024-07-06 14:20:48 +0530

    Time: 33 ms (66.33%) | Memory: 16.4 MB (77.72%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -1,4 +1,17 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
+        left_to_right = True
+        pos = 1
+        for i in range(1, time+1):
+            if left_to_right:
+                pos += 1
+            else:
+                pos -= 1
+            if i%(n-1) == 0:
+                left_to_right = not left_to_right
+        return pos
+
+""" 
         chunks = time // (n - 1)
         return (time % (n - 1) + 1) if chunks % 2 == 0 else (n - time % (n - 1))   
+"""
