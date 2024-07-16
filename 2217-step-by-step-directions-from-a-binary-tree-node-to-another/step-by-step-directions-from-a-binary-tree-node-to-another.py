bc140b21f0d376a914ad268483623c8388bde252
Rupesh Ravate <ravatez@gmail.com>
2024-07-16 20:11:21 +0530

    Time: 247 ms (85.91%) | Memory: 52.3 MB (42.28%) - LeetSync

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -1,3 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        
+        def find(node, val, path):
+            if node.val == val:
+                return True
+            if node.left and find(node.left, val, path):
+                path.append('L')
+                return True
+            if node.right and find(node.right, val, path):
+                path.append('R')
+                return True
+            return False
+        p1 = []
+        p2 = []
+        find(root, startValue, p1)
+        find = find(root, destValue, p2)
+        while p1 and p2 and p1[-1] == p2[-1]:
+            p1.pop()
+            p2.pop()
+        return 'U' * len(p1) + ''.join(p2[::-1])
+
+"""
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
         q: list[TreeNode] = [root]
@@ -61,3 +90,5 @@ class Solution:
 
         result_path.reverse()
         return "".join(result_path)
+        
+"""
