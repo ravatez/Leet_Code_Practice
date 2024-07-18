16ed8708d4a7b49d7141f691fc3f6ce4c56caac1
Rupesh Ravate <ravatez@gmail.com>
2024-07-18 21:06:59 +0530

    Time: 71 ms (94.75%) | Memory: 17 MB (22.83%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
@@ -1,3 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.res = 0
+
+        def solve(root):
+            if root is None:
+                return {}
+            if root.left is None and root.right is None:
+                # leaf node here
+                return {1: 1}
+            lhNodes = solve(root.left)            
+            rhNodes = solve(root.right)
+            
+            for leftNodeHeight in lhNodes:
+                for rightNodeHeight in rhNodes:
+                    if leftNodeHeight + rightNodeHeight <= distance:
+                        self.res += lhNodes[leftNodeHeight] * rhNodes[rightNodeHeight]
+
+            nhNodes = {}
+            for key in lhNodes:
+                if key <= distance:
+                    nhNodes[key + 1] = lhNodes[key]
+            for key in rhNodes:
+                if key <= distance:
+                    nhNodes[key + 1] = nhNodes.get(key + 1, 0) + rhNodes[key]                    
+
+
+
+            return nhNodes
+
+        solve(root)
+        return self.res
+
+"""
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
         self.map = {}
@@ -26,3 +66,4 @@ class Solution:
             return
         self.findLeaves(node.left, tmp, leaves, map)
         self.findLeaves(node.right, tmp, leaves, map)
+"""
