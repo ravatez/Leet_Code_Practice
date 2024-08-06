8f6e6bf8469dfb4852a151536436f9d9faa93530
Rupesh Ravate <ravatez@gmail.com>
2024-08-06 09:02:04 +0530

    Time: 102 ms (99.05%) | Memory: 17.6 MB (73.03%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,5 +1,15 @@
 class Solution:
-    def minimumPushes(self, input_text: str) -> int:
+    def minimumPushes(self, word: str) -> int:
+        l=[0]*(32)
+        for i in range(26):
+            l[i]=word.count(chr(97+i))
+        l.sort(reverse=True)
+        res=0
+        for i in range(4):
+            for j in range(8):
+                res+=(i+1)*l[8*i+j]
+        return res
+"""
         # Count letter occurrences
         letter_counts = [0] * 26
         for char in input_text:
@@ -15,3 +25,4 @@ class Solution:
             total_key_presses += (index // 8 + 1) * count
         
         return total_key_presses
+"""
