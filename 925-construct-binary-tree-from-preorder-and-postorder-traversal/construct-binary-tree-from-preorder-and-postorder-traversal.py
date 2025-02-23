9e5dd7b33953ea7e4559642380d820735ab609d5
Rupesh Ravate <ravatez@gmail.com>
2025-02-23 20:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (71.53%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,23 @@
+class Solution:
+    def constructFromPrePost(self, pre: List[int], post: List[int]) -> Optional[TreeNode]:
+        index = [0]  # Mutable index to track preorder position
+        return self.construct(pre, post, index, 0, len(pre) - 1)
+
+    def construct(self, pre, post, index, l, h):
+        if index[0] >= len(pre) or l > h:
+            return None
+
+        root = TreeNode(pre[index[0]])
+        index[0] += 1
+        if l == h:
+            return root
+
+        i = l
+        while i <= h and post[i] != pre[index[0]]:
+            i += 1
+
+        if i <= h:
+            root.left = self.construct(pre, post, index, l, i)
+            root.right = self.construct(pre, post, index, i + 1, h - 1)
+
+        return root
