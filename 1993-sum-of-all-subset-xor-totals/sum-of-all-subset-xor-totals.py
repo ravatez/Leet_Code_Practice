9b2e63808790614902a506726928eaba1417c198
Rupesh Ravate <ravatez@gmail.com>
2025-04-05 18:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (57.17%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,11 +1,3 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def dfs(i: int, xors: int) -> int:
-            if i == len(nums):
-                return xors
-
-            x = dfs(i + 1, xors)
-            y = dfs(i + 1, nums[i] ^ xors)
-            return x + y
-        
-        return dfs(0, 0)
+        return reduce(or_, nums)<<(len(nums)-1)
