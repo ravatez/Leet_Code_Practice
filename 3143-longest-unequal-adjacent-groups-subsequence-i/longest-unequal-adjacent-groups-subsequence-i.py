5b882f0d548efa219013bb4c6b2730dedd46c7f8
Rupesh Ravate <ravatez@gmail.com>
2025-05-15 18:19:35 +0530

    Time: 2 ms (31.46%) | Memory: 12.5 MB (50.56%) - LeetSync

---
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getLongestSubsequence(self, words, groups):
+        res = [words[0]]  # Start with the first word
+        for i in range(1, len(groups)):
+            if groups[i] != groups[i - 1]:
+                res.append(words[i])  # Add word if groups differ
+        return res
+
+        
